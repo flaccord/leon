11aa64db013ffd3d20790ecafef68f690ea5915b
flaccord <rincon.javier@gmail.com>
2022-11-24 17:28:52 -0600

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 define('CLIENT_ID', '86wluahn9m77um');
 define('CLIENT_SECRET', 'Tp9m8CH7oGBkInCq');
-define('REDIRECT_URL', 'https://upper.mx.rinconsedano.com/callback.php');
+define('REDIRECT_URL', 'https://upper.mx.rinconsedano.com/leon/callback.php');
 define('SCOPES', 'r_emailaddress,r_liteprofile,w_member_social');
